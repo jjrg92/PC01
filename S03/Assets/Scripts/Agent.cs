b2a0ef550b8fd6b1fe52a0dfeb79411a95cf6eef
MoyraRodriguez <moyrarodriguez@gmail.com>
2019-09-09 21:41:02 -0500

    fdgfd

---
+++ S03/Assets/Scripts/Agent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    Vector3 velocity, steer, desired = Vector3.zero;
+    float maxSpeed=10;
+    float maxSteer=10;
+    public bool alejar=false;
+
+    Transform target;
+
+    void Update()
+    {
+        target=GameObject.Find("Target").transform;
+        
+        if(alejar==true)
+        {
+            //Se calcula el deseado (desired)
+            desired=-(target.position-transform.position).normalized*maxSpeed;
+        }
+        else
+        {
+            //Se calcula el deseado (desired)
+            desired=(target.position-transform.position).normalized*maxSpeed;
+        }
+        
+        //Calculamos el steer
+        steer=Vector3.ClampMagnitude(desired-velocity,maxSteer);
+
+        velocity+=steer*Time.deltaTime;
+        transform.position+=velocity*Time.deltaTime;
+    }
+
+}
---
+++ S03/Assets/Scripts/Scripts PC1/Agent1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent1 : MonoBehaviour
+{
+    Vector3 velocity, steer, desired = Vector3.zero;
+    float maxSpeed=10;
+    float maxSteer=10;
+
+    Transform mouse;
+    bool victoria=false;
+
+    void Update()
+    {
+        mouse=GameObject.Find("Mouse").transform;
+        
+        desired=-(mouse.position-transform.position).normalized*maxSpeed;
+
+        steer=Vector3.ClampMagnitude(desired-velocity,maxSteer);
+
+        velocity+=steer*Time.deltaTime;
+
+        transform.position+=velocity*Time.deltaTime;
+
+        Vector3 cameraPosition=Camera.main.ScreenToWorldPoint(transform.position);
+        
+        if(transform.position.x >= -1 && transform.position.x <= 1 && transform.position.y >= -1 && transform.position.y <= 1)
+        {
+            Debug.Log("You Win");
+            Destroy(gameObject);
+            victoria=true;
+        }
+
+   }
+
+   void OnBecameInvisible()
+   {
+        if(victoria==false)
+        {
+            Debug.Log("You Lose");
+            Destroy(gameObject);
+        }
+   }
+
+}
